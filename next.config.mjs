147696443c8062b58779e214fa7cac689cf905b7
Nusrat Jahan Lia <nusratlia424@gmail.com>
2025-08-27 15:25:07 +0600

    next export err

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,17 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   eslint: {
+//     ignoreDuringBuilds: true,
+//   },
+//   typescript: {
+//     ignoreBuildErrors: true,
+//   },
+//   images: {
+//     unoptimized: true,
+//   },
+// }
+
+// export default nextConfig
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -7,8 +21,9 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    unoptimized: true,
+    unoptimized: true, // needed for <Image> in static export
   },
+  output: 'export', // enables static HTML export for GitHub Pages or other static hosts
 }
 
 export default nextConfig
